50d7f353fe5821e826a9dcb128b863e0c7eb56e0
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-05-05 00:13:49 +0530

    Update average salary.cpp

--- average salary.cpp
+++ average salary.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        int min=salary[0];
-        int max=salary[0];
-        int sum=0;
+        double min=salary[0];
+        double max=salary[0];
+        double sum=0;
         for(int i=0;i<salary.size();i++)
         {  
            if(salary[i]>max)
